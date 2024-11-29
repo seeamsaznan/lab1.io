5c9cd6b4d24b735eee760268ec188a56b4e4cd73
Seeam Saznan <saznanseeam@gmail.com>
2024-11-28 21:57:56 -0500

    first 2 part

--- Assignment 4/Part 2/gallery-start/main.js
+++ Assignment 4/Part 2/gallery-start/main.js
@@ -1,18 +1,35 @@
+// Array of image filenames
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+// Object of alternative text for each image
+const altText = {
+  'pic1.jpg': 'Closeup of a human eye',
+  'pic2.jpg': 'Mountain view',
+  'pic3.jpg': 'Ocean sunset',
+  'pic4.jpg': 'City skyline',
+  'pic5.jpg': 'Forest trail'
+};
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+// Loop through the image filenames array to create thumbnails
+imageFilenames.forEach((filename) => {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${filename}`);
+  newImage.setAttribute('alt', altText[filename]);
+  thumbBar.appendChild(newImage);
 
-/* Declaring the alternative text for each image file */
+  // When a thumbnail is clicked, update the displayed image
+  newImage.addEventListener('click', () => {
+    displayedImage.setAttribute('src', `images/${filename}`);
+    displayedImage.setAttribute('alt', altText[filename]);
+  });
+});
 
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+// Button to toggle the darken effect
+btn.addEventListener('click', () => {
+  overlay.style.backgroundColor = overlay.style.backgroundColor === 'rgba(0,0,0,0.7)' ? 'rgba(0,0,0,0)' : 'rgba(0,0,0,0.7)';
+});
