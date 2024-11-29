3d9de4f926f521cd4f5138ecb9de9e49cb98baa8
Seeam Saznan <saznanseeam@gmail.com>
2024-11-29 00:20:31 -0500

    Update main.js

--- Assignment 4/Part 3/main.js
+++ Assignment 4/Part 3/main.js
@@ -28,4 +28,32 @@ class Ball {
     this.size = size;
   }
 
-  
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -Math.abs(this.velX);
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+ 
